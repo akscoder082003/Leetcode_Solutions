8f76db7e7da55b8d964bc3aaf59e3c3b9d1c2e6f
akscoder082003 <ashishk.singh1008@gmail.com>
2024-01-06 13:33:40 +0530

    solved

---
+++ MySQL/1661.  Average Time of Process per Machine.sql
@@ -0,0 +1,36 @@
+-- Table: Activity
+
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | machine_id     | int     |
+-- | process_id     | int     |
+-- | activity_type  | enum    |
+-- | timestamp      | float   |
+-- +----------------+---------+
+-- The table shows the user activities for a factory website.
+-- (machine_id, process_id, activity_type) is the primary key (combination of columns with unique values) of this table.
+-- machine_id is the ID of a machine.
+-- process_id is the ID of a process running on the machine with ID machine_id.
+-- activity_type is an ENUM (category) of type ('start', 'end').
+-- timestamp is a float representing the current time in seconds.
+-- 'start' means the machine starts the process at the given timestamp and 'end' means the machine ends the process at the given timestamp.
+-- The 'start' timestamp will always be before the 'end' timestamp for every (machine_id, process_id) pair.
+
+
+-- There is a factory website that has several machines each running the same number of processes. Write a solution to find the average time each machine takes to complete a process.
+
+-- The time to complete a process is the 'end' timestamp minus the 'start' timestamp. The average time is calculated by the total time to complete every process on the machine divided by the number of processes that were run.
+
+-- The resulting table should have the machine_id along with the average time as processing_time, which should be rounded to 3 decimal places.
+
+select a1.machine_id, 
+round(avg(a2.timestamp - a1.timestamp), 3) 
+as processing_time 
+from Activity a1 
+join Activity a2 
+on a1.machine_id = a2.machine_id 
+and a1.process_id = a2.process_id 
+and a1.activity_type = 'start' 
+and a2.activity_type = 'end' 
+group by a1.machine_id;
