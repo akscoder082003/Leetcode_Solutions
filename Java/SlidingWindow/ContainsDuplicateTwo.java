8b9976b6c30a8774ff44aa3039d010f2db699976
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-10-13 18:40:11 +0530

    Added Sliding Window problem-solution to Leetcode_Solutions.

---
+++ Java/SlidingWindow/ContainsDuplicateTwo.java
@@ -0,0 +1,31 @@
+/*
+ * PROBLEM: 219
+ * 
+ * Given an integer array nums and an integer k, return true if there are two
+ * distinct indices i and j in the array such that nums[i] == nums[j] and abs(i
+ * - j) <= k.
+ * 
+ * Example 1:
+ * Input: nums = [1,2,3,1], k = 3
+ * Output: true
+ * 
+ * Example 2:
+ * Input: nums = [1,0,1,1], k = 1
+ * Output: true
+ * 
+ * Example 3:
+ * Input: nums = [1,2,3,1,2,3], k = 2
+ * Output: false
+ */
+class ContainsDuplicateTwo {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && Math.abs(map.get(nums[i]) - i) <= k) {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
