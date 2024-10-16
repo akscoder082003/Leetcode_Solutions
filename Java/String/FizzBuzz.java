a94bc311cf00a5230fba86c24a86e4048e0b9f28
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-10-16 10:02:30 +0530

    Added String Problem-Solution
    Co-authored-by: Ashish Kumar Singh ‹ashishk.singh1008@gmail.com›
    Co-authored-by: Laxman Singh Koranga <laxmankoranga03@gmail.com>

---
+++ Java/String/FizzBuzz.java
@@ -0,0 +1,20 @@
+/*
+ * PROBLEM: 412 : Fizz Buzz
+ */
+class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                result.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.add("Fizz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(String.valueOf(i));
+            }
+        }
+        return result;
+    }
+}
