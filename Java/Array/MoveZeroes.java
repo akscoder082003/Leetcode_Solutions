4ceed7ee765af2d97b588bc492c100cb78a715ac
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-10-16 10:49:19 +0530

    Added Array Problem-Solution.
    Co-authored-by: Ashish Kumar Singh <ashishk.singh1008@gmail.com>
    Co-authored-by: Laxman Singh Koranga <laxmankoranga03@gmail.com>

---
+++ Java/Array/MoveZeroes.java
@@ -0,0 +1,18 @@
+/*
+ * PROBLEM: 283: Move Zeroes
+ */
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int startIdx = 0;
+        int endIdx = 0;
+        while (endIdx < nums.length) {
+            if (nums[endIdx] != 0) {
+                nums[startIdx++] = nums[endIdx];
+            }
+            endIdx++;
+        }
+        while (startIdx < nums.length) {
+            nums[startIdx++] = 0;
+        }
+    }
+}
