c6f130e86081e70a0c03ece0ecd4a6132b130263
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-10-16 10:18:28 +0530

    Added HashTable Problem-Solution
    Co-authored-by: Ashish Kumar Singh <ashishk.singh1008@gmail.com>
    Co-authored-by: Laxman Singh Koranga <laxmankoranga03@gmail.com>

---
+++ Java/HashTable/HappyNumber.java
@@ -0,0 +1,24 @@
+/*
+ * PROBLEM: 202 : Happy Number
+ */
+class HappyNumber {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = getSquareDigitSum(n);
+        while (fast != 1 && slow != fast) {
+            slow = getSquareDigitSum(slow);
+            fast = getSquareDigitSum(getSquareDigitSum(fast));
+        }
+        return fast == 1;
+    }
+
+    private int getSquareDigitSum(int n) {
+        int squareDigitSum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            squareDigitSum += digit * digit;
+            n /= 10;
+        }
+        return squareDigitSum;
+    }
+}
